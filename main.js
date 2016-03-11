0bfabff124f574ff1897a6e2a04e600685925000
Andrew <andrewsy@live.com>
2016-03-10 23:30:09 -0800

    training for state

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import ReactDom from 'react-dom';
 // import App from './App.jsx';
 // import App from './JSX/App.jsx';
 // import App from './component/App.jsx';
-import App2 from './component/App2.jsx';
+// import App2 from './component/App2.jsx';
+import App from './state/App.jsx';
 
-ReactDom.render(<App2 />, document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById('app'));
