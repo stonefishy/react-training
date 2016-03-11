77221dfcac2b7e5b7e7315d3dceb7bc19ce03ec7
Andrew <andrewsy@live.com>
2016-03-10 23:00:41 -0800

    react training - data

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 
 // import App from './App.jsx';
 // import App from './JSX/App.jsx';
-import App from './component/App.jsx';
+// import App from './component/App.jsx';
+import App2 from './component/App2.jsx';
 
 ReactDom.render(<App2 />, document.getElementById('app'));
