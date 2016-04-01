1d6dbeb332d859b0a5d2c521bdbc93bcdb1ebc32
Andrew <andrewsy@163.com>
2016-04-01 23:13:11 +0800

    flux: added reducers

---
+++ flux/reducers/reducers.js
@@ -0,0 +1,30 @@
+import {combineReducers} from 'redux';
+import {ADD_TODO} from '../actions/action.js';
+
+function todo(state, action) {
+	switch(action.type) {
+		case ADD_TODO:
+			return {
+				id: action.id,
+				text: action.text
+			}
+
+		default:
+			return state;
+	}
+}
+
+function todos(state, action) {
+	switch(action.type) {
+		case ADD_TODO:
+			return [
+				...state,
+				todo(undefined, action)
+			]
+
+		default:
+			return state
+	}
+}
+
+export const todoApp = combineReducers({todos});
